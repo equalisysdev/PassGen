60248af1b1471f50720a9e168c5578bec92b1d80
Equalisys <thdrroi@gmail.com>
2023-05-11 18:45:15 +0200

    Ajoutez des fichiers projet.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace PassGenerator;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
